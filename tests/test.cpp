8d5fec51f77e51724f1351aeb4657d526494395d
Thomaltarix <thomas.boue@epitech.eu>
2024-04-28 18:51:36 +0200

    :white_check_mark: Add a simple test to pass the actions

---
+++ tests/test.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** RayTracer
+** File description:
+** tests
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(tests, test1)
+{
+    return;
+}
