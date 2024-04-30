1d2067a826884e18eb94de6b6159179b5b4736e1
Thomaltarix <thomas.boue@epitech.eu>
2024-04-30 15:37:12 +0200

    :memo: Add documentation for the Primitive interface

--- include/primitives/IPrimitives.hpp
+++ include/primitives/IPrimitives.hpp
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include "Ray.hpp"
+#include "Point3D.hpp"
+
 namespace RayTracer {
     /**
      * @class IPrimitives
@@ -20,43 +23,37 @@ namespace RayTracer {
         virtual ~IPrimitives() = default;
 
         /**
-         * @brief Checks if a ray intersects with the primitive.
-         *
-         * @param ray The ray to check intersection with.
-         * @param t The parameter representing the intersection point along the ray.
-         * @return True if the ray intersects with the primitive, false otherwise.
+         * @brief Checks if a ray hits the primitive.
+         * @param ray The ray to check for hits.
+         * @return True if the ray hits the primitive, false otherwise.
          */
         virtual bool hits(const Math::Ray &ray) = 0;
 
         /**
          * @brief Sets the position of the primitive.
-         *
          * @param x The x-coordinate of the position.
          * @param y The y-coordinate of the position.
          * @param z The z-coordinate of the position.
          */
-        // virtual void setPos(float x, float y, float z) = 0;
+        virtual void setPos(float x, float y, float z) = 0;
 
         /**
          * @brief Sets the position of the primitive.
-         *
          * @param point The position as a Point3D object.
          */
-        // virtual void setPos(Point3D point) = 0;
+        virtual void setPos(Math::Point3D point) = 0;
 
         /**
          * @brief Gets the position of the primitive.
-         *
          * @return The position of the primitive as a Point3D object.
          */
-        // virtual Point3D getPos() = 0;
+        virtual Math::Point3D getPos() = 0;
 
         /**
          * @brief Gets the normal vector at a given point on the primitive.
-         *
          * @param point The point on the primitive.
          * @return The normal vector at the given point.
          */
-        // virtual Vector3D getNormalAt(const Vector3D &point) = 0;
+        virtual Math::Vector3D getNormalAt(const Math::Vector3D &point) = 0;
     };
 }
