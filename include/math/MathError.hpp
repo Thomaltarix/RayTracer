cf2171495d02528f6a197ffdf58ef5bf6b561ee1
Thomaltarix <thomas.boue@epitech.eu>
2024-05-02 17:43:55 +0200

    :memo: Add documentation for the Math Error class

--- include/math/MathError.hpp
+++ include/math/MathError.hpp
@@ -11,11 +11,26 @@
 #include <string>
 
 namespace Math {
+    /**
+     * @brief Exception class for mathematical errors.
+     */
     class MathError : public std::exception {
     public:
+        /**
+         * @brief Constructs a MathError object with the given error message.
+         * @param message The error message.
+         */
         MathError(std::string const &message);
+
+        /**
+         * @brief Returns a C-style string describing the exception.
+         * @return A C-style string describing the exception.
+         */
         const char *what() const noexcept override;
+
     private:
+
+        /**< The error message. */
         std::string _message;
     };
 }
