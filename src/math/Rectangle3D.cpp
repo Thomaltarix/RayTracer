dd725c3de8651cb7220095a1ff1d95a60d662588
landryarki <landry.gigant@epitech.eu>
2024-05-05 17:06:39 +0200

    :ambulance: Rectangle method pointAt added to namespace Math

--- src/math/Rectangle3D.cpp
+++ src/math/Rectangle3D.cpp
@@ -8,7 +8,7 @@
 #include "Rectangle3D.hpp"
 #include "MathError.hpp"
 
-Math::Point3D Rectangle3D::pointAt(double u, double v) const
+Math::Point3D Math::Rectangle3D::pointAt(double u, double v) const
 {
     if (u < 0 || u > 1 || v < 0 || v > 1) {
         throw Math::MathError("u and v must be between 0 and 1");
