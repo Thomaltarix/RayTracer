6624271d2c242ad9132f19819979ffde1136bb65
Aubane <aubane.nourry@epitech.eu>
2024-05-06 15:51:47 +0200

    :memo: Add documentation to the .hpp files

--- src/plugins/materials/flatColor/FlatColor.hpp
+++ src/plugins/materials/flatColor/FlatColor.hpp
@@ -10,10 +10,30 @@
 #include "AMaterial.hpp"
 
 namespace Material {
+
+    /**
+     * @brief The FlatColor class represents a material with a flat color.
+     *
+     * This class inherits from the AMaterial class and provides a simple material
+     * implementation with a single flat color.
+     */
     class FlatColor : public RayTracer::AMaterial {
         public:
+        /**
+         * @brief Default constructor for the FlatColor class.
+         */
         FlatColor();
+
+        /**
+         * @brief Constructor for the FlatColor class with a specified color.
+         *
+         * @param color The color of the material.
+         */
         FlatColor(Math::Vector3D color);
+
+        /**
+         * @brief Destructor for the FlatColor class.
+         */
         ~FlatColor();
     };
 }
