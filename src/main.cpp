570b61fc28f9a0940e85d1157b19594f3c293a65
Marius-P1 <marius.pain@epitech.eu>
2024-05-07 20:04:31 +0200

    :sparkles: Update main.cpp to handle the Core and Scene initialization

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,21 @@
 ** main
 */
 
+#include "core/Core.hpp"
+#include "core/Scene.hpp"
+
 int main(int ac, char **av)
 {
-    (void) ac;
-    (void) av;
+    if (ac != 2) {
+        std::cerr << "Usage: ./raytracer [scene.cfg]" << std::endl;
+        return 84;
+    }
+    try {
+        RayTracer::Core core;
+        RayTracer::Scene scene(av[1], &core);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 84;
+    }
     return 0;
 }
