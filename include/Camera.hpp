20d437b8da27e99b266a5d6f91efe549c98abdeb
landryarki <landry.gigant@epitech.eu>
2024-05-07 17:25:34 +0200

    :memo: Add documentation for the Camera and Image classes

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -11,15 +11,50 @@
 #include "Rectangle3D.hpp"
 
 namespace RayTracer {
+    /**
+     * @class Camera
+     * @brief This class represents a camera in the RayTracer project.
+     *
+     * A Camera object contains an origin point and a screen rectangle.
+     * It provides methods to create a default camera, create a camera with specific parameters, and generate a ray.
+     */
     class Camera {
     public:
+        /**
+         * @brief The origin point of the camera in 3D space.
+         */
         Math::Point3D origin;
+
+        /**
+         * @brief The screen rectangle that the camera is capturing.
+         */
         Math::Rectangle3D screen;
 
+        /**
+         * @brief Default constructor. Creates a default camera.
+         */
         Camera();
+
+        /**
+         * @brief Constructor. Creates a camera with the specified origin and screen.
+         *
+         * @param origin The origin point of the camera in 3D space.
+         * @param screen The screen rectangle that the camera is capturing.
+         */
         Camera(const Math::Point3D &origin, const Math::Rectangle3D &screen);
+
+        /**
+         * @brief Default destructor.
+         */
         ~Camera() = default;
 
+        /**
+         * @brief Generates a ray from the camera through a point on the screen.
+         *
+         * @param u The horizontal coordinate of the point on the screen.
+         * @param v The vertical coordinate of the point on the screen.
+         * @return The generated ray.
+         */
         [[nodiscard]] Math::Ray ray(double u, double v) const;
     };
 }
--- include/Image.hpp
+++ include/Image.hpp
@@ -14,18 +14,65 @@
 #include <string>
 
 namespace RayTracer {
+    /**
+     * @class Image
+     * @brief This class represents an image in the RayTracer project.
+     *
+     * An Image object contains a camera, a list of primitives, a list of lights, and dimensions (width and height).
+     * It provides methods to create an empty image, create an image with specific parameters, and render the image to a file.
+     */
     class Image {
     public:
+        /**
+         * @brief The camera used to capture the image.
+         */
         Camera _camera;
+
+        /**
+         * @brief A list of primitives (shapes) in the image.
+         */
         std::vector<std::shared_ptr<IPrimitive>> _primitives;
+
+        /**
+         * @brief A list of lights in the image.
+         */
         std::vector<std::shared_ptr<ILight>> _lights;
+
+        /**
+         * @brief The width of the image in pixels.
+         */
         std::size_t _width;
+
+        /**
+         * @brief The height of the image in pixels.
+         */
         std::size_t _height;
 
+        /**
+         * @brief Default constructor. Creates an empty image.
+         */
         Image();
+
+        /**
+         * @brief Constructor. Creates an image with the specified camera, primitives, and lights.
+         *
+         * @param camera The camera used to capture the image.
+         * @param primitives A list of primitives (shapes) in the image.
+         * @param lights A list of lights in the image.
+         */
         Image(const Camera &camera, const std::vector<std::shared_ptr<IPrimitive>> &primitives, const std::vector<std::shared_ptr<ILight>> &lights);
+
+        /**
+         * @brief Default destructor.
+         */
         ~Image() = default;
 
+        /**
+         * @brief Renders the image to a file.
+         *
+         * @param filename The name of the file to which the image is rendered.
+         */
         void render(std::string filename);
     };
 }
+
