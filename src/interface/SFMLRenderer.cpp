9e5c3a002486997ded3405752730ace6a5fa9b8b
Aubane <aubane.nourry@epitech.eu>
2024-05-10 16:17:48 +0200

    :building_construction: Put SFMLRenderer in RayTracer namespace

--- src/interface/SFMLRenderer.cpp
+++ src/interface/SFMLRenderer.cpp
@@ -7,29 +7,29 @@
 
 #include "SFMLRenderer.hpp"
 
-SFMLRenderer::SFMLRenderer()
+RayTracer::SFMLRenderer::SFMLRenderer()
 {
     window.create(sf::VideoMode(800, 600), "RayTracer");
 }
 
-SFMLRenderer::SFMLRenderer(int width, int height)
+RayTracer::SFMLRenderer::SFMLRenderer(int width, int height)
 {
     window.create(sf::VideoMode(width, height), "RayTracer");
 }
 
-SFMLRenderer::SFMLRenderer(const std::string &filename)
+RayTracer::SFMLRenderer::SFMLRenderer(const std::string &filename)
 {
     this->loadPPM(filename);
 }
 
-void SFMLRenderer::display()
+void RayTracer::SFMLRenderer::display()
 {
     window.clear();
     window.draw(this->getSprite());
     window.display();
 }
 
-void SFMLRenderer::loadPPM(const std::string &filename)
+void RayTracer::SFMLRenderer::loadPPM(const std::string &filename)
 {
     _image.loadFromFile(filename);
     _texture.loadFromImage(_image);
@@ -37,57 +37,57 @@ void SFMLRenderer::loadPPM(const std::string &filename)
     window.create(sf::VideoMode(_image.getSize().x, _image.getSize().y), "RayTracer");
 }
 
-sf::Image SFMLRenderer::getImage() const
+sf::Image RayTracer::SFMLRenderer::getImage() const
 {
     return _image;
 }
 
-void SFMLRenderer::setImage(const sf::Image &image)
+void RayTracer::SFMLRenderer::setImage(const sf::Image &image)
 {
     _image = image;
 }
 
-sf::Texture SFMLRenderer::getTexture() const
+sf::Texture RayTracer::SFMLRenderer::getTexture() const
 {
     return _texture;
 }
 
-void SFMLRenderer::setTexture(const sf::Texture &texture)
+void RayTracer::SFMLRenderer::setTexture(const sf::Texture &texture)
 {
     _texture = texture;
 }
 
-sf::Sprite SFMLRenderer::getSprite() const
+sf::Sprite RayTracer::SFMLRenderer::getSprite() const
 {
     return _sprite;
 }
 
-void SFMLRenderer::setSprite(const sf::Sprite &sprite)
+void RayTracer::SFMLRenderer::setSprite(const sf::Sprite &sprite)
 {
     _sprite = sprite;
 }
 
-int SFMLRenderer::getWidth() const
+int RayTracer::SFMLRenderer::getWidth() const
 {
     return _width;
 }
 
-void SFMLRenderer::setWidth(int width)
+void RayTracer::SFMLRenderer::setWidth(int width)
 {
     _width = width;
 }
 
-int SFMLRenderer::getHeight() const
+int RayTracer::SFMLRenderer::getHeight() const
 {
     return _height;
 }
 
-void SFMLRenderer::setHeight(int height)
+void RayTracer::SFMLRenderer::setHeight(int height)
 {
     _height = height;
 }
 
-SFMLRenderer::~SFMLRenderer()
+RayTracer::SFMLRenderer::~SFMLRenderer()
 {
     window.close();
 }
