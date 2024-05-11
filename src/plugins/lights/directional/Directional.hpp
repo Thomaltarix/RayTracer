498792138b9eea4dbe96103a65ada5a6af54d575
Aubane <aubane.nourry@epitech.eu>
2024-05-11 12:36:44 +0200

    :memo: Add documentation to Directional light class header

--- src/plugins/lights/directional/Directional.hpp
+++ src/plugins/lights/directional/Directional.hpp
@@ -10,19 +10,78 @@
 #include "ALight.hpp"
 
 namespace Light {
+    /**
+     * @brief The Directional class represents a directional light source in the RayTracer.
+     *
+     * It inherits from the ALight class and provides functionality to set the direction of the light source,
+     * calculate illumination at a given point, and check if a point is in shadow.
+     */
     class Directional : public RayTracer::ALight {
     public:
+        /**
+         * @brief Default constructor for the Directional class.
+         */
         Directional();
+
+        /**
+         * @brief Constructor for the Directional class.
+         *
+         * @param intensity The intensity of the light source.
+         */
         Directional(float intensity);
+
+        /**
+         * @brief Constructor for the Directional class.
+         *
+         * @param intensity The intensity of the light source.
+         * @param direction The direction of the light source.
+         */
         Directional(float intensity, Math::Vector3D direction);
+
+        /**
+         * @brief Constructor for the Directional class.
+         *
+         * @param direction The direction of the light source.
+         */
         Directional(Math::Vector3D direction);
+
+        /**
+         * @brief Default destructor for the Directional class.
+         */
         ~Directional() = default;
 
+        /**
+         * @brief Get the direction of the light source.
+         *
+         * @return The direction vector of the light source.
+         */
         Math::Vector3D getDirection() const;
+
+        /**
+         * @brief Set the direction of the light source.
+         *
+         * @param direction The new direction vector of the light source.
+         */
         void setDirection(Math::Vector3D direction);
+
+        /**
+         * @brief Check if a given point is in shadow.
+         *
+         * @param point The point to check.
+         * @return True if the point is in shadow, false otherwise.
+         */
         bool InShadow(Math::Point3D point) override;
+
+        /**
+         * @brief Calculate the illumination at a given point.
+         *
+         * @param point The point to calculate illumination for.
+         * @param material The material of the object at the point.
+         * @return The illumination vector at the given point.
+         */
         Math::Vector3D Illuminate(Math::Point3D point, const std::shared_ptr<RayTracer::IMaterial> &material) override;
+
     private:
-        Math::Vector3D _direction;
+        Math::Vector3D _direction; /**< The direction vector of the light source. */
     };
 }
